ddbff00556d25254a0d25c50228d0479acbb027e
Sibusiso <Sibusisodyan@gmail.com>
2023-06-10 14:43:30 +0200

    Added_animation

---
+++ script/products.js
@@ -0,0 +1,30 @@
+let products_output = document.querySelector('.container_output');
+let displayProducts =[
+    {
+        image:'https://i.postimg.cc/q7mHkVNm/coming-soon3.jpg',
+    },
+    {
+        image:'https://i.postimg.cc/q7KXy7Zt/shoe10.jpg',
+    },
+    {
+        image:'https://i.postimg.cc/XYGw6JW2/shoe6.jpg',
+    },
+   
+]
+
+displayProducts.forEach(run => {
+   products_output.innerHTML+=`
+   <div class="col-4 text-center" style="border:1px solid black;">
+                  <div class="card">
+                    <img src="${run.image}" class="card-img-top" alt="..." style=" object-fit:contain;
+                    aspect-ratio: 3/2;">
+                    <div class="card-body">
+                      <h5 class="card-title">Card title</h5>
+                      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                      <button>add to cart</button>
+                    </div>
+                  </div>
+                </div>
+                <br>
+   ` 
+});
