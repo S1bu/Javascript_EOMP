0a4e8a017b76dc4f3800819b47abac66341fd870
S1bu <Sibusisodyan@gmail.com>
2023-06-12 13:50:47 +0200

    product section done

--- script/admin.js
+++ script/admin.js
@@ -16,7 +16,8 @@ function display(){
                 <td class="td">${display.title}</td>
                 <td class="td">R ${display.price}</td>
                 <td class="td">${display.description}</td>
-                <td class="td"><img src="${display.image}"></td>
+                <td class="td"><img src="${display.image}"style=" object-fit:contain;
+                aspect-ratio: 3/3;"></td>
                 <td class="td"><button>edit</button></td> <!---edit product-->
                 <td class="td"><button onclick="removeBtn()">del</button></td> <!---delete product-->
            
@@ -60,98 +61,7 @@ function removeBtn() {
 // location.reload(); //reloads the page
 }
 
-
-// function deleteBook() {
-//   const bookId = prompt("Enter book ID:");
-  
-//   const bookIndex = books.findIndex(book => book.bookId === bookId);
-  
-//   if (bookIndex !== -1) {
-//     books.splice(bookIndex, 1);
-//     console.log("Book deleted successfully!");
-//   } else {
-//     console.log("Book not found!");
-//   }
-// }
-
-
-/**
- * let books = []; // Array to store books
-
-function createBook() {
-  const bookId = prompt("Enter book ID:");
-  const title = prompt("Enter book title:");
-  const author = prompt("Enter book author:");
-  
-  const book = { bookId, title, author };
-  books.push(book);
-  
-  console.log("Book created successfully!");
-}
-
-function readBook() {
-  const bookId = prompt("Enter book ID:");
-  
-  const book = books.find(book => book.bookId === bookId);
-  
-  if (book) {
-    console.log("Title: " + book.title);
-    console.log("Author: " + book.author);
-  } else {
-    console.log("Book not found!");
-  }
-}
-
-function updateBook() {
-  const bookId = prompt("Enter book ID:");
-  
-  const bookIndex = books.findIndex(book => book.bookId === bookId);
-  
-  if (bookIndex !== -1) {
-    const title = prompt("Enter new title (leave empty to keep the existing title):");
-    const author = prompt("Enter new author (leave empty to keep the existing author):");
-    
-    if (title) {
-      books[bookIndex].title = title;
-    }
-    if (author) {
-      books[bookIndex].author = author;
-    }
-    
-    console.log("Book updated successfully!");
-  } else {
-    console.log("Book not found!");
-  }
-}
-
-
-// Main program loop
-while (true) {
-  const choice = prompt("Choose an operation: 1. Create, 2. Read, 3. Update, 4. Delete, 5. Exit");
-  
-  switch (choice) {
-    case "1":
-      createBook();
-      break;
-    case "2":
-      readBook();
-      break;
-    case "3":
-      updateBook();
-      break;
-    case "4":
-      deleteBook();
-      break;
-    case "5":
-      console.log("Exiting program.");
-      return;
-    default:
-      console.log("Invalid choice. Please try again.");
-  }
-}
- * 
- */
-
+ 
 
 
 // DELETE FROM CRUD --CHATGPT
@@ -173,72 +83,9 @@ while (true) {
 
 // // Usage example
 // deleteData("123"); // Delete data with identifier "123"
-
-
-//UPDATE FROM CRUD
-// Example of a simple CRUD system with an array as the data store
-// let dataStore = [];
-
-// // Update operation
-// function updateData(identifier, newData) {
-//   const index = dataStore.findIndex(item => item.id === identifier);
-  
-//   if (index !== -1) {
-//     dataStore[index] = { ...dataStore[index], ...newData };
-//     console.log("Data updated successfully!");
-//   } else {
-//     console.log("Data not found!");
-//   }
-// }
-
-// // Usage example
-// const newData = { name: "John Doe", age: 30 };
-// updateData("123", newData); // Update data with identifier "123" using new data
-
-
-// let firstExample = [
-//   {
-//   id:1,
-//   name:'Sibusiso'
-//   },
-//   {
-//   id:2,
-//   name:'Taflynne'
-//   },
-//   {
-//   id:3,
-//   name:'Kgodisho'
-//   },
-//   {
-//   id:4,
-//   name:'Rabia'
-//   },
-//   {
-//   id:5,
-//   name:'Amy'
-//   },
-//   ];
-  
-  
-  
-//   let num = parseInt(prompt('Enter your wish to find'));
-//   let newName =prompt(`name`);
-  
-//   function findIndex(index){ //my find the index function
-//     return index.id === num;  //looking
-//   }
-//   console.log(firstExample) //display full data
-  
-//   let target = firstExample.find(findIndex); //display everything of that object
-  
-//   // firstExample.push(target);
-//   // firstExample[firstExample.length + 1] = target;
-//   // let editName = prompt('name enter')
-//   function edit(name){
-    
-//     this.name = name;
-//   }
-  
-//   target = new edit(`${name}`)
+ 
   
 //   console.log(firstExample)//display full data again
+
+
+ 
--- script/products.js
+++ script/products.js
@@ -1,4 +1,7 @@
 let products_output = document.querySelector('.container_output');
+let products_output2 = document.querySelector('.container_output2');
+let list = JSON.parse(localStorage.getItem('id')) ? JSON.parse(localStorage.getItem('id')) : [];
+// list of default products
 let displayProducts =[
     {
         image:'https://i.postimg.cc/q7mHkVNm/coming-soon3.jpg',
@@ -12,24 +15,34 @@ let displayProducts =[
    
 ]
 
+// The default products
 displayProducts.forEach(run => {
    products_output.innerHTML+=`
-   <div class="col-4 text-center" class="displayProducts" style="border:1px solid black;">
-                  <div class="card">
-                    <img src="${run.image}" class="card-img-top" alt="..." style=" object-fit:contain;
-                    aspect-ratio: 3/2;">
-                    <div class="card-body">
-                      <h5 class="card-title">Card title</h5>
-                      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                      <button style="background-color:#3F3D3D;
-                      color: white;
-                      font-weight: 900;
-                      width: 40%;
-                      border: 1px solid black;
-                      margin: 4px;padding:10px">add to cart</button>
-                    </div>
-                  </div>
-                </div>
-                <br>
+   <div class="card" width="300px">
+   <img src="${run.image}" class="card-img-top" alt="..." style=" object-fit:contain;
+   aspect-ratio: 3/2;">
+   <div class="card-body">
+     <h5 class="card-title">Card title</h5>
+     <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+     <button class="add_TO_CART">add to cart</button>
+   </div>
+ </div>
+
    ` 
 });
+//Products from the local storage
+list.forEach(run => {
+  products_output2.innerHTML+=`
+<div class="card" width="300px">
+<img src="${run.image}" cla.ss="card-img-top" alt="..." style=" object-fit:contain;
+aspect-ratio: 3/2;">
+<div class="card-body">
+  <h5 class="card-title">${run.title}</h5>
+  <p class="card-text">${run.description}</p>
+  <p class="card-text"><b>R${run.price}</b><p>
+  <button class="add_TO_CART">add to cart</button>
+</div>
+</div>
+  ` 
+});
+ 
